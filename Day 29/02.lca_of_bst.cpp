7cc69ea191e49eaaf3c4aa70a42bec5101f4d14f
oswalgopal <gploswal@gmail.com>
2022-04-21 15:16:15 +0530

    added day 29

---
+++ Day 29/02.lca_of_bst.cpp
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+// LCA for BST
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) {
+            return NULL;
+        }
+        if (root->val > p->val && root->val > q->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        if (root->val < p->val && root->val < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        return root;
+    }
+};
+
+
+// LCA for binary tree
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) {
+            return NULL;
+        }
+        if (root==p || q == root) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) {
+            return root;
+        } else if (left) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+};
